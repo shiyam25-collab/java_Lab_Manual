a067d832f924844e042c1e6e9cc51ef29bab8457
shiyam25-collab <shiyamluffy25@gmail.com>
2026-07-25 09:29:00 +0530

    Add files via upload

---
+++ 705-Design Hashset.java
@@ -0,0 +1,24 @@
+Program :
+
+//705-Design hashset
+class MyHashSet {
+
+    private boolean[] set;
+
+    public MyHashSet() {
+        set = new boolean[1000001];
+    }
+
+    public void add(int key) {
+        set[key] = true;
+    }
+
+    public void remove(int key) {
+        set[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
+ 
